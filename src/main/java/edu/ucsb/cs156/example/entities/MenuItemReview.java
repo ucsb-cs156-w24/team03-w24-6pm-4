b1d6998e35854621f0176486414bb801f5a44553
Luxiang Zhang <luxiang@ucsb.edu>
2024-02-15 16:04:09 -0800

    lz - Add backend of MenuItemReview from team02

---
+++ src/main/java/edu/ucsb/cs156/example/entities/MenuItemReview.java
@@ -0,0 +1,26 @@
+package edu.ucsb.cs156.example.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "menuitemreview")
+public class MenuItemReview {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String diningCommonsCode;
+  private String name;  
+  private String station;
+}
