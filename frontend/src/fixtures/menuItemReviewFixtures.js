4794bea0373b06b23e838b6b9efc819277a7be4a
Luxiang Zhang <luxiang@ucsb.edu>
2024-02-15 17:10:31 -0800

    lz - add fixtures for menuItemReview

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,34 @@
+const menuItemReviewFixtures = {
+    oneReview:
+    [
+        {
+            "id": 1,
+            "diningCommonsCode": "ortega",
+            "name": "Apple Pie",
+            "station": "Desserts"
+        }
+    ],
+    threeReviews:
+    [
+        {
+            "id": 1,
+            "diningCommonsCode": "ortega",
+            "name": "Apple Pie",
+            "station": "Desserts"
+        },
+        {
+            "id": 2,
+            "diningCommonsCode": "portola",
+            "name": "Turkey Sandwich",
+            "station": "Sandwiches"
+        },
+        {
+            "id": 3,
+            "diningCommonsCode": "de-la-guerra",
+            "name": "Latte",
+            "station": "Beverages"
+        }
+    ]
+};
+
+export { menuItemReviewFixtures };
