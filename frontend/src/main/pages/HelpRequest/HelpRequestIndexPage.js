80f3aa71a668e6122adf2f98bc84e52f77f2a867
ryanvo504 <ryanvo@ucsb.edu>
2024-02-19 02:20:25 -0800

    fix navigation error

--- frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.js
+++ frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.js
@@ -24,7 +24,7 @@ export default function HelpRequestIndexPage() {
         return (
             <Button
                 variant="primary"
-                href="/helprequests/create"
+                href="/helprequest/create"
                 style={{ float: "right" }}
             >
                 Create HelpRequest
