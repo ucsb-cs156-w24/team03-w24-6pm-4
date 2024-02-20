5d56180736a9fb4d8b3c46edf3131ab74d3b0146
Aavash <aavash@ucsb.edu>
2024-02-19 18:00:18 -0800

    finished articles

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,45 @@
+const articlesFixtures = {
+    oneArticle:
+    [
+      {
+       "id": 1,
+        "title": "Article1",
+        "url": "Article1.com",   
+        "explanation": "First Article",
+        "email": "Article1@gmail.com",
+        "dateAdded": "2022-01-02T12:00:00"
+      }
+    ],
+
+    threeArticles:
+    [
+        {
+            "id": 2,
+            "title": "Article2",
+            "url": "Article2.com",   
+            "explanation": "Second Article",
+            "email": "Article2@gmail.com",
+            "dateAdded": "2022-01-02T12:00:00"        },
+
+        {
+            "id": 3,
+            "title": "Article3",
+            "url": "Article3.com",   
+            "explanation": "Third Article",
+            "email": "Article3@gmail.com",
+            "dateAdded": "2022-04-03T12:00:00" 
+        },
+
+        {
+            "id": 4,
+            "title": "Article4",
+            "url": "Article4.com",   
+            "explanation": "Fourth Article",
+            "email": "Article4@gmail.com",
+            "dateAdded": "2022-07-04T12:00:00"      
+        },
+        
+    ]
+};
+
+export { articlesFixtures };
---
+++ frontend/src/main/components/Articles/ArticlesForm.js
@@ -0,0 +1,170 @@
+import { Button, Form, Row, Col } from 'react-bootstrap';
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+
+function ArticlesForm({ initialContents, submitAction, buttonLabel = "Create" }) {
+
+    // Stryker disable all
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm(
+        { defaultValues: initialContents || {}, }
+    );
+    // Stryker restore all
+
+    const navigate = useNavigate();
+
+    // For explanation, see: https://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
+    // Note that even this complex regex may still need some tweaks
+
+    // Stryker disable next-line Regex
+   
+
+    return (
+
+        <Form onSubmit={handleSubmit(submitAction)}>
+
+
+            <Row>
+
+                {initialContents && (
+                    <Col>
+                        <Form.Group className="mb-3" >
+                            <Form.Label htmlFor="id">Id</Form.Label>
+                            <Form.Control
+                                data-testid="ArticlesForm-id"
+                                id="id"
+                                type="text"
+                                {...register("id")}
+                                value={initialContents.id}
+                                disabled
+                            />
+                        </Form.Group>
+                    </Col>
+                )}
+
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="title">Title</Form.Label>
+                        <Form.Control
+                            data-testid="ArticlesForm-title"
+                            id="title"
+                            type="text"
+                            isInvalid={Boolean(errors.text)}
+                            {...register("title", { required: "Title is required", maxLength: { value: 30, message: "Max length 30 characters"}})}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.title?.message }
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                </Row>
+
+                <Row>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="url">URL</Form.Label>
+                        <Form.Control
+                            data-testid="ArticlesForm-url"
+                            id="url"
+                            type="text"
+                            isInvalid={Boolean(errors.url)}
+                            {...register("url", { required: "URL is required"})}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.url?.message}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+            </Row>
+
+            <Row>
+
+                <Col>
+
+
+
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="explanation">Explanation</Form.Label>
+                        <Form.Control
+                            data-testid="ArticlesForm-explanation"
+                            id="explanation"
+                            type="text"
+                            isInvalid={Boolean(errors.explanation)}
+                            {...register("explanation", {
+                                required: "Explanation is required."
+                            })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.explanation?.message}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+            </Row>
+
+            <Row>
+
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="email">Email</Form.Label>
+                        <Form.Control
+                            data-testid="ArticlesForm-email"
+                            id="email"
+                            type="text"
+                            isInvalid={Boolean(errors.email)}
+                            {...register("email", {
+                                required: "Email is required."
+                            })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.email?.message}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                </Row>
+            
+                <Row>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="dateAdded">Date Added</Form.Label>
+                        <Form.Control
+                            data-testid="ArticlesForm-dateAdded"
+                            id="dateAdded"
+                            type="datetime-local"
+                            isInvalid={Boolean(errors.dateAdded)}
+                            {...register("dateAdded", {
+                                required: "Date Added is required."
+                            })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.dateAdded?.message}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                    <Button
+                        type="submit"
+                        data-testid="ArticlesForm-submit"
+                    >
+                        {buttonLabel}
+                    </Button>
+                    <Button
+                        variant="Secondary"
+                        onClick={() => navigate(-1)}
+                        data-testid="ArticlesForm-cancel"
+                    >
+                        Cancel
+                    </Button>
+                </Col>
+            </Row>
+        </Form>
+
+    )
+}
+
+export default ArticlesForm;
